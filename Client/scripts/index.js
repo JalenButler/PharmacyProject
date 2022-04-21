e9e2d3a3c54ec76b40b9838dca3cb2aae24cfc3b
jmmalcom <jmmalcom@crimson.ua.edu>
2022-04-21 18:20:52 -0500

    Login

--- Client/scripts/index.js
+++ Client/scripts/index.js
@@ -1,9 +1,2 @@
 const baseUrl = "";
 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
